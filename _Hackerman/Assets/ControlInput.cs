ae8a9b52edc135b04e6632af50a30c2c98d5470d
Jackobi <jackobi.forsyth@gmail.com>
2018-06-12 21:46:29 +1000

    Initial Commit
    -Added the base project
    -Character controls are read via the camera in place of the character
    itself, and passed on to the character for interpretation

---
+++ _Hackerman/Assets/ControlInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlInput : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mainCharRef;
+    private GameObject possessionRef;
+
+	void Awake ()
+    {
+        mainCharRef = GameObject.Find("CharacterRobotBoy");
+	}
+	
+	
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.T) == true)
+        {
+            
+        }
+	}
+}
